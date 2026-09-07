8089ff567ad5b82ce411a42ede4bf96ecf008dcd
sandro <santamariaramos18@hotmail.com>
2026-09-07 11:11:15 -0500

    enviando datos por formulario

--- 03-Formularios/app.py
+++ 03-Formularios/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,8 +6,15 @@ app = Flask(__name__)
 def inicio():
     return render_template('base.html')
 
-@app.route('/auth/register/')
+@app.route('/auth/register/', methods=['GET','POST'])
 def registrar():
+    
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form['username']
+        password = request.form['password']
+        return f'Nombre de usuario {username} y la contraseña {password}'
+    
     return render_template('auth/register.html')
     
 if __name__ == '__main__':
