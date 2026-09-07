a71de23172eee355d55aa7c5d8a13e5c47e75cc4
sandro <santamariaramos18@hotmail.com>
2026-09-06 23:48:33 -0500

    creacion de plantilla jinja html y enviar variables por render_template()

--- 02-Jinja2-HTML/app.py
+++ 02-Jinja2-HTML/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def incio():
-    return ' Hola Mundo '
+def index():
+    return render_template('index.html')
+
+@app.route('/mensaje')
+def mensaje():
+    nombre = 'Sandro'
+    return render_template('mensaje.html', nombreHtml = nombre)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
