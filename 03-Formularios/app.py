287855395bfb28a05d6923ad75b29359a03198e4
sandro <santamariaramos18@hotmail.com>
2026-09-07 11:22:20 -0500

    validando formularios

--- 03-Formularios/app.py
+++ 03-Formularios/app.py
@@ -13,8 +13,22 @@ def registrar():
         print(request.form)
         username = request.form['username']
         password = request.form['password']
-        return f'Nombre de usuario {username} y la contraseña {password}'
-    
+        if len(username) >= 4  and len(username)<=25 and len(password)>=2 and len(password)<=40:
+            #return f'Nombre de usuario {username} y la contraseña {password}'
+            success =f""" 
+                Usuario y contraseña 
+                usuario {username}
+                password {password}
+            """
+            return render_template('auth/register.html',successHtml = success)
+        else:
+            error =""" 
+                Nombre de usuario debe tener entre 4 y 25 caracteres y la 
+                contraseña debe tener entre 2 a 40 caracteres.            
+            """
+            return render_template('auth/register.html',errorHtml = error)
+            
+        
     return render_template('auth/register.html')
     
 if __name__ == '__main__':
