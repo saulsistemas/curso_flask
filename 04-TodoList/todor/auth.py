4cbe9bf7a0b727ce2890ee8ff5b3ff448ed4e1ea
sandro <santamariaramos18@hotmail.com>
2026-09-08 17:38:56 -0500

    plantilla login

--- 04-TodoList/todor/auth.py
+++ 04-TodoList/todor/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 bp =  Blueprint('auth',__name__,url_prefix='/auth/')
 
 @bp.route('/register/')
 def register():
-    return 'Registrar usuario'
+    return render_template('auth/register.html')
 
 @bp.route('/login/')
 def login():
-    return 'iniciar sesión'
+    return render_template('auth/login.html')
